662f371b0773223fbf2f9a965af7c8f285ac3315
Halil Çalışkan <72007910+caliskanhalil@users.noreply.github.com>
2023-11-02 21:08:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ function changeText() {
     const paragraph = document.querySelector('p');
     paragraph.textContent = "You clicked the button!";
 }
+
+ document.getElementById('emailForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const userEmail = document.getElementById('email').value;
+    // Burada normalde sunucuya e-postayı göndermek için bir işlem yapılabilir, ancak bu sadece bir örnek olduğu için işlem yapılmayacak.
+    document.getElementById('confirmationMessage').innerText = `E-posta adresiniz: ${userEmail}. Aboneliğiniz başarıyla alındı.`;
+    document.getElementById('email').value = ''; // Formu temizle
+  });
